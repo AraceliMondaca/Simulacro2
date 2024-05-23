f43e3b5361c95f4fafa725e5a6bc253b8b20ba5f
Araceli Mondaca <80856098+AraceliMondaca@users.noreply.github.com>
2024-05-23 17:24:56 -0300

    Update Cliente.php

--- Cliente.php
+++ Cliente.php
@@ -1,101 +1,108 @@
-?php 
-class Cliente{
-private $tipodocumento;
-private $nrodocumento;
-private $nombre;
-private $apellido;
-private $telefono;
+<?php 
 
-    public function __construct($tipodocumento,$nrodocumento,$nombre,$apellido, $telefono){
+class Venta{
+private $numero;
+private $fecha;
+private $objCliente;
+private $objColMoto;
+private $precioFinal;
+function __construct($numero,$fecha,$objCliente,$objColMoto,$precioFinal){
+$this->numero=$numero;
+$this->fecha=$fecha;
+$this->objCliente=$objCliente=array($objCliente);
+$this->objColMoto=$objColMoto=array($objColMoto);
+$this->precioFinal=$precioFinal;
+}
+public function getNumero(){
+    return $this->numero;
+}
+public function getFecha(){
+    return $this->fecha;
+}
+public function getObjCliente(){
+    return $this->objCliente;
+}
+public function getObjColMoto(){
+    return $this->objColMoto;
+}
+public function getPrecioFinal(){
+    return $this->precioFinal;
+}
+public function setNumero($numero){
+    $this->numero=$numero;
+}
 
-        $this-> tipodocumento=$tipodocumento;
-        
-        $this-> nrodocumento=$nrodocumento;
-        
-        $this-> nombre=$nombre;
-        
-        $this-> apellido=$apellido;
-        
-        $this-> telefono=$telefono;
-        
+public function setFecha($fecha){
+    $this->fecha=$fecha;
+}
+public function setObjCliente($objCliente){
+    $this->objCliente=$objCliente;
+}
+public function setObjColMoto($objColMoto){
+    $this->objColMoto=$objColMoto;
+}
+public function setPrecioFinal($precioFinal){
+    $this->precioFinal=$precioFinal;
+}
+
+
+public function incorporarMoto($objColMoto){
+$precioVenta= $objColMoto->darPrecioVenta();
+$ColeccionMotos= $this->getObjColMoto();
+    if ($precioVenta>0) {
+       array_push($ColeccionMotos,$objColMoto);
+$this->setObjColMoto($ColeccionMotos);
+$this->setPrecioFinal($this->getPrecioFinal()+$precioVenta);
+    }
+    
+}
+
+public function retornarTotalVentaNacional() {
+    $total = 0;
+    foreach ($this->objColMoto as $moto) {
+        if ($moto instanceof MotoNacionales) {
+            if($moto->darPrecioVenta()>0){
+                $total += $moto->darPrecioVenta();
         }
+        }
+}
+    return $total;
+}
+public function retornarMotosImportadas(){
+    $importadas = [];
+    foreach ($this->objColMoto as $moto){
+        if ($moto instanceof MotoImportadas){
+            $importadas[] = $moto;
+        }
+}
+    return $importadas;
+}
 
-    public function getTipodocumento() {
-        return $this->tipodocumento;
+private function arregloString($array) {
+    $cadena = '';
+    if (is_array($array)) {
+        foreach ($array as $elemento) {
+            if (is_array($elemento)) {
+                $cadena .= $this->arregloString($elemento);
+            } else {
+                $cadena .= $elemento . "\n";
+            }
+        }
     }
-    public function setTipodocumento($tipodocumento){
-        $this->tipodocumento = $tipodocumento;
-
-    }
-
-/**
- * Get the value of nrodocumento
- */ 
-public function getNrodocumento()
-{
-return $this->nrodocumento;
-}
-
-/**
- * Set the value of nrodocumento
- *
- * @return  self
- */ 
-public function setNrodocumento($nrodocumento)
-{
-$this->nrodocumento = $nrodocumento;
-
-return $this;
-}
-
-/**
- * Get the value of nombre
- */ 
-public function getNombre()
-{
-return $this->nombre;
-}
-
-/**
- * Set the value of nombre
- *
- * @return  self
- */ 
-public function setNombre($nombre)
-{
-$this->nombre = $nombre;
-
-return $this;
-}
-
-
-public function getApellido(){
-return $this->apellido;
-}
-
-public function setApellido($apellido)
-{
-$this->apellido = $apellido;
-}
-
- 
-public function getTelefono(){
-return $this->telefono;
-}
-
-public function setTelefono($telefono){
-$this->telefono = $telefono;
-
+    return $cadena; 
 }
 public function __toString(){
-
-    $info= "\n".
-    "Tipo de Documento: " .$this->getTipodocumento() ."\n" .
-    "Numero de Documento: " .$this->getNrodocumento() ."\n".
-    "Nombre :" .$this->getNombre() . "\n".
-    "Apellido :" .$this->getApellido() ."\n".
-    "telefono ." .$this->getTelefono();
-    return $info;
+    $stringCliente=$this->arregloString($this->getObjCliente());
+    $stringMoto=$this->arregloString($this->getObjColMoto());
+    $venta="   INFORMACIÓN DE LA VENTA   \n". 
+    "Numero: ".$this->getNumero()."\n". 
+    "Fecha: ".$this->getFecha()."\n". 
+    "\n".$stringCliente."\n". 
+    "\n".$stringMoto."\n". 
+    "Precio Final: ".$this->getPrecioFinal();
+    return $venta;
 }
+
+
 }
 ?>
